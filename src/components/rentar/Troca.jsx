5cbdee14c015931c78e65aec04a1ae60f72b03ba
alexis romero mendoza <alexis.rm162917@gmail.com>
2024-02-27 18:11:26 -0700

    adding more styles to cart

--- src/components/rentar/Troca.jsx
+++ src/components/rentar/Troca.jsx
@@ -10,17 +10,21 @@ export const Troca = () => {
       <label htmlFor={trocaCheckBoxId} className='cart-button'>
         <FaTruckMonster />
       </label>
-      <input type="checkbox" hidden id={trocaCheckBoxId}/>
+      <input type='checkbox' hidden id={trocaCheckBoxId} />
 
-      <div className='cart'>
+      <div className='cart'>        
         <h2>Que llevas en la troca</h2>
-        <ul>
+        <ul className='cartList'>
           <li>mesa grande - 180</li>
           <li>mesa grande - 120</li>
           <li>mesa grande - 150</li>
           <li>mesa grande - 180</li>
         </ul>
-        <p>total = $650</p>
+        <hr />
+        <p className='textNormal'>Total a pagar:</p>
+        <p className='totalCost'>$650</p>
+        <button className='deleteTrocaButton'>Borrar carga</button>
+        <button className='goToRentarButton'>Proceder con la renta</button>
       </div>
     </div>
   );
